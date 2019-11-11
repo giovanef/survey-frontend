d4a8c234ac3388a82e670f4d2d94f5bee72ec10f
Giovane Ferreira <giovaneferreira93@gmail.com>
2019-11-10 21:14:38 -0300

    criando página e funcionalidade de criação de enquetes

--- src/views/PoolNew.vue
+++ src/views/PoolNew.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <h1>Pool New</h1>
+  <div class="pools">
+    <pools-form></pools-form>
   </div>
 </template>
+
+<script>
+import PoolsForm from '@/components/features/PoolsForm.vue';
+
+export default {
+  components: {
+    PoolsForm,
+  },
+};
+</script>
